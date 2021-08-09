8aac64af47d7d9bdd2f9b74e2ad286bd64fa672f
Antonio Mendoza <antoniomendoza05@gmail.com>
2021-08-08 19:10:25 -0700

    website mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Antonio
+        <img src={"a73244b4779c4f45a102801396a4568c.png"} class="headerpic" />
+        Mendoza
       </header>
+      <div className="Tonypic">
+        <img src={"NY-TO-NY.jpg"} />
+      </div>
+      <section>
+        <h2>Profiles and Resume</h2>
+        <a
+          href="https://www.linkedin.com/in/antoniomendoza1234561944"
+          class="button"
+        >
+          Linkedin Profile
+        </a>
+        <br />
+        <a className="button" href="https://github.com/am852410">
+          Github Profile
+        </a>
+        <br />
+        <a
+          className="button"
+          href="https://drive.google.com/file/d/1lGHC_3tJO85GCthHwiy_HMlGUwhtNB4o/view?usp=sharing"
+        >
+          Resume
+        </a>
+      </section>
+      <h2>Projects</h2>
+      <a
+        className="button"
+        href="https://peacemakerdogwalkingcompany.herokuapp.com/"
+      >
+        Dog Walking Website
+      </a>
+      <br />
+      <br />
+      <a
+        className="button"
+        href="https://craft-tacos-and-beer-united.herokuapp.com/recipes"
+      >
+        Taco Recipe Website
+      </a>
+      <section />
     </div>
   );
 }
